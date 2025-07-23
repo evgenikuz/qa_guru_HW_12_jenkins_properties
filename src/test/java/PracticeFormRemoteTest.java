f22727945400c96b03858ed49b2155acfb30f354
Evgeniia Kharitonova <kuznetsovaevgeni@gmail.com>
2025-07-23 20:19:06 +0300

    removed comment

--- src/test/java/PracticeFormRemoteTest.java
+++ src/test/java/PracticeFormRemoteTest.java
@@ -36,7 +36,6 @@ public class PracticeFormRemoteTest {
                 Configuration.browserCapabilities = options;
             }
         }
-//        Configuration.browserVersion = System.getProperty("version", "128.0");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@" + System.getProperty("selenoid_url") + "/wd/hub";
